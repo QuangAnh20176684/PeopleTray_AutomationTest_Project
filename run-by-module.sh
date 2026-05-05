69f971c0d7d3779e86923adc12ed130d39839d54
Nguyễn Quang Anh <mun141999@gmail.com>
2026-05-05 15:13:53 +0700

    update flow

--- run-by-module.sh
+++ run-by-module.sh
@@ -168,6 +168,7 @@ FACTS=$(cat <<EOF
   {"name":"[Skipped]","value":"${SKIPPED}"},
   {"name":"Run by",        "value":"${RUN_BY}@${HOSTNAME_VAL}"},
   {"name":"Time",          "value":"${START_TIME}"},
+  {"name":"Report",          "value":"https://quanganh20176684.github.io/PeopleTray_AutomationTest_Project/"},
 
 ]
 EOF
